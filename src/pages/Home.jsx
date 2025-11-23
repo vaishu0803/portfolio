249d018a3ac1b0d9e9f482381c64509903ce4a50
vaishu0803 <vaishuyenumula@gmail.com>
2025-11-24 00:19:51 +0530

    Initial portlio commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import "../CSS/Home.css"
 import '../index.css' 
 
 // 🖼️ Import Assets
-import photo from '../../public/photo.jpg'
+import photo from '../../public/profile.jpeg'
 import githubLogo from '../../public/github.png'
 import linkedinLogo from '../../public/linkedin.png'
 import gmailLogo from '../../public/gmail.png'
